6acd6f332aa0dab6d97af81cef1b4e1dea5ff6d1
Douda Bensasson <dbensasson@uga.edu>
2016-11-17 11:42:30 -0500

    added an option to allow multiple replicate runs with same value of K

--- structureShell.pl
+++ structureShell.pl
@@ -18,14 +18,18 @@ my $L = 53;
 my $N = 83;
 my $maxK = 15;
 my $minK = 11;
+my $replicates = 1;
 
-getopts('i:L:N:s:e:',\%parameters);
+getopts('i:L:N:s:e:r:S',\%parameters);
 
 if (exists $parameters{"i"}) { $infile = $parameters{"i"}; }
 if (exists $parameters{"L"}) { $L = $parameters{"L"}; }
 if (exists $parameters{"N"}) { $N = $parameters{"N"}; }
 if (exists $parameters{"s"}) { $minK = $parameters{"s"}; }
 if (exists $parameters{"e"}) { $maxK = $parameters{"e"}; }
+if (exists $parameters{"r"}) { $replicates = $parameters{"r"}; }
+
+
 
 unless (exists $parameters{"i"}) {
 	print "\n USAGE: $program -i <infile>\n\n";
@@ -33,32 +37,62 @@ unless (exists $parameters{"i"}) {
 	print   "    -i\tinfile for structure\n";
 	print   "    -s\tstarting K [$minK]\n";
 	print   "    -e\tending K [$maxK]\n";
+	print   "    -r\tnumber of times to repeat STRUCTURE run from starting K to ending K [$replicates]\n";
+	print   "    -S\tuse with -r to change seed for each replicate run of STRUCTURE\n";
 	print   "    -L\tnumber of loci [$L]\n";
 	print   "    -N\tnumber of individuals [$N]\n\n";
 	exit;
 }
 
-for (my $k=$minK; $k<=$maxK; $k++) {
-	open OUT, ">$infile.k$k.structure.stdout" or die "couldn't open $infile.k$k.structure : $!";
-	print "\nK = $k\n"; 
-	open MAINPARAMS, "<mainparams" or die "couldn' open mainparams : $!";
-	open TEMP, ">temp" or die "couldn't open temp : $!"; 
-	while (<MAINPARAMS>) {
-		if (/#define MAXPOPS/) { print TEMP "#define MAXPOPS    $k      // (int) number of populations assumed\n"; }
-		if (/#define MAXPOPS/) { print "#define MAXPOPS    $k      // (int) number of populations assumed\n"; }
-		else { print TEMP $_; }
-	}
-	close TEMP;
-	close MAINPARAMS;
-#	system ("cat mainparams");
-	`cp temp mainparams`;
+for (my $r=1; $r<=$replicates; $r++) {			# run structure $replicates times 
 
-	my $structureout = `structure -i $infile -L $L -N $N`;
+	my $seed = int(rand(10000));
 
-	while ($structureout =~ /Estimated Ln Prob of Data   = (\S+)/g) {
-		print "Estimated Ln Prob of Data   = $1\n";
+	for (my $k=$minK; $k<=$maxK; $k++) {		# from $minK to $maxK
+		my $outfile;
+		if ($replicates > 1) { 
+			if (defined $parameters{'s'}) { $outfile = "$infile.k$k"."_$r"."_$seed.structure"; }
+			else { $outfile = "$infile.k$k"."_$r.structure"; }
+		}
+		else { $outfile = "$infile.k$k.structure"; }
+		open OUT, ">$outfile.stdout" or die "couldn't open $outfile.stdout : $!";
+		print "\nK = $k\n";
+		if ($replicates > 1) { print "Replicate $r\n"; }
+		open MAINPARAMS, "<mainparams" or die "couldn' open mainparams : $!";	# change K (MAXPOPS) in mainparams
+		open TEMP, ">temp" or die "couldn't open temp : $!"; 
+		while (<MAINPARAMS>) {
+			if (/#define MAXPOPS/) { print TEMP "#define MAXPOPS    $k      // (int) number of populations assumed\n"; }
+			if (/#define MAXPOPS/) { print "#define MAXPOPS    $k      // (int) number of populations assumed\n"; }
+			else { print TEMP $_; }
+		}
+		close TEMP;
+		close MAINPARAMS;
+
+		`cp temp mainparams`;
+
+		if (defined $parameters{'s'}) {
+			open EXTRAPARAMS, "<extraparams" or die "couldn' open extraparams : $!";	# change seed in extraparams
+			open TEMP, ">temp" or die "couldn't open temp : $!"; 
+		
+			while (<EXTRAPARAMS>) {
+
+				if (/#define SEED/) { print TEMP "#define SEED    $seed      //  (int) seed value for random number generator perl: int(rand(10000))\n"; }
+				if (/#define SEED/) { print "#define SEED    $seed      // (int) seed value for random number generator perl: int(rand(10000))\n"; }
+				else { print TEMP $_; }
+			}
+			close TEMP;
+			close EXTRAPARAMS;
+
+			`cp temp extraparams`;
+		}
+
+		my $structureout = `structure -i $infile -L $L -N $N`;
+
+		while ($structureout =~ /Estimated Ln Prob of Data   = (\S+)/g) {
+			print "Estimated Ln Prob of Data   = $1\n";
+		}
+	 	print OUT $structureout;
+		close OUT;
+		`cp outfile_f $outfile`;
 	}
- 	print OUT $structureout;
-	close OUT;
-	`cp outfile_f $infile.k$k.structure`;
 }
