c6270dc0f2ad3e2532557aff6acbd8940e8ab39e
Douda Bensasson <dbensasson@uga.edu>
2018-03-13 10:22:40 -0400

    example data for faChrompaint

---
+++ fastatools/faChrompaintData/nearestP34048.R
@@ -0,0 +1,58 @@
+
+rm(list=ls())
+pdf("P34048paintchre1AA.pdf")
+data<-read.table("P34048.nearest.tsv",header=T)
+attach(data)
+head(data)
+tail(data)
+
+par(mfrow=c(8,1),mar=c(1,1,1,1),yaxt='n',xlab='',bty="n")
+
+
+
+# PRINT STRAIGHTFORWARD GENOME PLOTS USING USER-SPECIFIED COLOUR FOR BINS WITH TIED MATCHES
+# data from P34048.nearest.tsv
+plot(c(0,max(winpos)),c(0,100),type="n",main="P34048.chr1.mfa",yaxt='n',xaxt='n')
+rect(winpos[infile=="chr1.mfa"]-100000,0,winpos[infile=="chr1.mfa"],100,col=as.vector(cladecolor[infile=="chr1.mfa"]))
+
+
+# PRINT STRAIGHTFORWARD GENOME PLOTS USING USER-SPECIFIED COLOUR FOR BINS WITH TIED MATCHES
+# data from P34048.nearest.tsv
+plot(c(0,max(winpos)),c(0,100),type="n",main="P34048.chr2.mfa",yaxt='n',xaxt='n')
+rect(winpos[infile=="chr2.mfa"]-100000,0,winpos[infile=="chr2.mfa"],100,col=as.vector(cladecolor[infile=="chr2.mfa"]))
+
+
+# PRINT STRAIGHTFORWARD GENOME PLOTS USING USER-SPECIFIED COLOUR FOR BINS WITH TIED MATCHES
+# data from P34048.nearest.tsv
+plot(c(0,max(winpos)),c(0,100),type="n",main="P34048.chr3.mfa",yaxt='n',xaxt='n')
+rect(winpos[infile=="chr3.mfa"]-100000,0,winpos[infile=="chr3.mfa"],100,col=as.vector(cladecolor[infile=="chr3.mfa"]))
+
+
+# PRINT STRAIGHTFORWARD GENOME PLOTS USING USER-SPECIFIED COLOUR FOR BINS WITH TIED MATCHES
+# data from P34048.nearest.tsv
+plot(c(0,max(winpos)),c(0,100),type="n",main="P34048.chr4.mfa",yaxt='n',xaxt='n')
+rect(winpos[infile=="chr4.mfa"]-100000,0,winpos[infile=="chr4.mfa"],100,col=as.vector(cladecolor[infile=="chr4.mfa"]))
+
+
+# PRINT STRAIGHTFORWARD GENOME PLOTS USING USER-SPECIFIED COLOUR FOR BINS WITH TIED MATCHES
+# data from P34048.nearest.tsv
+plot(c(0,max(winpos)),c(0,100),type="n",main="P34048.chr5.mfa",yaxt='n',xaxt='n')
+rect(winpos[infile=="chr5.mfa"]-100000,0,winpos[infile=="chr5.mfa"],100,col=as.vector(cladecolor[infile=="chr5.mfa"]))
+
+
+# PRINT STRAIGHTFORWARD GENOME PLOTS USING USER-SPECIFIED COLOUR FOR BINS WITH TIED MATCHES
+# data from P34048.nearest.tsv
+plot(c(0,max(winpos)),c(0,100),type="n",main="P34048.chr6.mfa",yaxt='n',xaxt='n')
+rect(winpos[infile=="chr6.mfa"]-100000,0,winpos[infile=="chr6.mfa"],100,col=as.vector(cladecolor[infile=="chr6.mfa"]))
+
+
+# PRINT STRAIGHTFORWARD GENOME PLOTS USING USER-SPECIFIED COLOUR FOR BINS WITH TIED MATCHES
+# data from P34048.nearest.tsv
+plot(c(0,max(winpos)),c(0,100),type="n",main="P34048.chr7.mfa",yaxt='n',xaxt='n')
+rect(winpos[infile=="chr7.mfa"]-100000,0,winpos[infile=="chr7.mfa"],100,col=as.vector(cladecolor[infile=="chr7.mfa"]))
+
+
+# PRINT STRAIGHTFORWARD GENOME PLOTS USING USER-SPECIFIED COLOUR FOR BINS WITH TIED MATCHES
+# data from P34048.nearest.tsv
+plot(c(0,max(winpos)),c(0,100),type="n",main="P34048.chrR.mfa",yaxt='n',xaxt='n')
+rect(winpos[infile=="chrR.mfa"]-100000,0,winpos[infile=="chrR.mfa"],100,col=as.vector(cladecolor[infile=="chrR.mfa"]))
